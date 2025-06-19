d74aee6ef37c870d7a77f16db4754db339190eef
leehak07 <leehak1228@dgsw.hs.kr>
2025-06-19 14:30:09 +0900

    작성페이지
    끝,.......이아닐수도

---
+++ src/routes/index.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../pages/homepage/HomePage";
+import SearchPage from "../pages/search/SearchPage";
+import StoryDetailPage from "../pages/storydetailpage/StoryDetailPage";
+import MyPage from "../pages/mypage/MyPage";
+import LoginPage from "../pages/authpage/Login";
+import SignupPage from "../pages/authpage/Signup";
+import PostsPage from "../pages/postpage/PostsPage";
+import EpisodeDetailPage from "../pages/episode/EpisodeDetailPage.jsx";
+// import NotFoundPage from "../pages/NotFoundPage";
+// import 
+
+const AppRoutes = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    // localStorage에서 로그인 상태 불러오기
+    const storedLoginStatus = localStorage.getItem("isLoggedIn");
+    if (storedLoginStatus === "true") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "true");
+  }
+  
+  // likedWorks
+  // myWorks
+  // profile
+  // recentViews
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
+  };
+
+
+  return (
+    <Routes>
+      <Route path="/main" element={<HomePage isLoggedIn={isLoggedIn} />} />
+      <Route path="/search" element={<SearchPage isLoggedIn={isLoggedIn} />} />
+      <Route path="/story/:note_id" element={<StoryDetailPage />} />{" "}
+      <Route path="/notes/:note_id/episodes/:episode_id" element={<EpisodeDetailPage />} />
+      <Route path="/" element={<LoginPage setIsLoggedIn={handleLogin} />} />
+      <Route path="/signup" element={<SignupPage setIsLoggedIn={setIsLoggedIn} />} />
+      {/* <Route path="*" element={<NotFoundPage />} /> */}
+      <Route path="/posts" element={<PostsPage isLoggedIn={isLoggedIn} />} />
+      <Route path="/mypage" element={<MyPage isLoggedIn={isLoggedIn} />} />
+      <Route path="/mypage/myworks" element={<MyWorksPage isLoggedIn={isLoggedIn} />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
